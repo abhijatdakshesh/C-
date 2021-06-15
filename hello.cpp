ad316ad934392392bb283c31af13c5507236558c
Amrendra <pathakamrendra@gmail.com>
2021-06-15 13:35:54 +0530

    hello programme

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    std::cout << "Hi my name is Amrendra\n";
+    printf("Hi I am printf function");
+    return 0;
+}
